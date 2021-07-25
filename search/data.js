efb7f6bf1e2fdd892aa498eeb909d1fafada2c82
peperon4141 <peperon4141@users.noreply.github.com>
2021-07-25 23:47:20 +0000

    deploy: 848376649cec43178617c71fa569d635816cd511

---
+++ search/data.js
@@ -0,0 +1,38 @@
+var data = [
+{
+url: "https://book.choihack.com/post/%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0%E3%82%B7%E3%83%B3%E3%82%AD%E3%83%B3%E3%82%B0%E5%85%A5%E9%96%80/",
+title: "システム・シンキング入門",
+date: "2021-07-25T09:03:54+09:00",
+body: "システム・シンキング入門 おすすめ度 Amazon システム・シンキング入門 (日経文庫) 新しい発見度 4 他の人に話したい度 3 テスト これはテストメッセージです。"
+},
+{
+url: "https://book.choihack.com/post/",
+title: "",
+date: "2021-07-25T09:03:54+09:00",
+body: ""
+},
+{
+url: "https://book.choihack.com/post/%E6%96%99%E7%90%86%E3%81%AE%E5%9B%9B%E9%9D%A2%E4%BD%93/",
+title: "料理の四面体 ― 東西美味発見法",
+date: "2021-07-25T09:03:54+09:00",
+body: "料理の四面体 ― 東西美味発見法 おすすめ度 Amazon 料理の四面体―東西美味発見法 (酒文ライブラリー) 新しい発見度 4 他の人に話したい度 3 テスト これはテストメッセージです。"
+},
+{
+url: "https://book.choihack.com/",
+title: "読書ログ",
+date: "2021-07-25T09:03:54+09:00",
+body: "読書ログ"
+},
+{
+url: "https://book.choihack.com/categories/",
+title: "Categories",
+date: "0001-01-01T00:00:00Z",
+body: "Categories"
+},
+{
+url: "https://book.choihack.com/tags/",
+title: "Tags",
+date: "0001-01-01T00:00:00Z",
+body: "Tags"
+},
+];
