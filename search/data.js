ea21a56f08396bfe45535158c34b19137abe407c
peperon4141 <peperon4141@users.noreply.github.com>
2021-08-10 22:54:12 +0000

    deploy: 1113df3a8e85c2b810d9f000b2a0e6464106db56

--- search/data.js
+++ search/data.js
@@ -1,102 +1,102 @@
 var data = [
 {
-url: "https://book.choihack.com/tags/",
+url: "https://review.choihack.com/tags/",
 title: "Tags",
 date: "2021-08-10T08:14:26+09:00",
 body: "Tags"
 },
 {
-url: "https://book.choihack.com/tags/%E3%83%9B%E3%83%A9%E3%83%BC/",
+url: "https://review.choihack.com/tags/%E3%83%9B%E3%83%A9%E3%83%BC/",
 title: "ホラー",
 date: "2021-08-10T08:14:26+09:00",
 body: "ホラー"
 },
 {
-url: "https://book.choihack.com/",
+url: "https://review.choihack.com/",
 title: "",
 date: "2021-08-10T08:14:26+09:00",
 body: ""
 },
 {
-url: "https://book.choihack.com/post/",
+url: "https://review.choihack.com/post/",
 title: "",
 date: "2021-08-10T08:14:26+09:00",
 body: ""
 },
 {
-url: "https://book.choihack.com/tags/%E6%98%A0%E7%94%BB/",
+url: "https://review.choihack.com/tags/%E6%98%A0%E7%94%BB/",
 title: "映画",
 date: "2021-08-10T08:14:26+09:00",
 body: "映画"
 },
 {
-url: "https://book.choihack.com/post/movie/",
+url: "https://review.choihack.com/post/movie/",
 title: "",
 date: "2021-08-10T08:14:26+09:00",
 body: ""
 },
 {
-url: "https://book.choihack.com/post/movie/%E7%8A%AC%E9%B3%B4%E6%9D%91/",
+url: "https://review.choihack.com/post/movie/%E7%8A%AC%E9%B3%B4%E6%9D%91/",
 title: "犬鳴村",
 date: "2021-08-10T08:14:26+09:00",
 body: "犬鳴村 評価 var ctx = document.getElementById(\"raderChart\"); var raderChart = new Chart(ctx, { type: 'radar', data: { labels: [ '演出','テンポ','こわさ' ], datasets: [{ data: [ 2,7,5 ], backgroundColor: 'RGBA(225,95,150, 0.5)', borderColor: 'RGBA(225,95,150, 1)', borderWidth: 1, pointBackgroundColor: 'RGB(46,106,177)' }] }, options: { legend: { display: false, }, scale:{ ticks:{ suggestedMin: 0, suggestedMax: 10, stepSize: 2 }, pointLabels: { fontSize: 16 } } } }); .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 犬鳴村 SNSや動画サイト上で恐怖の声が絶えない九州屈指の心霊スポット・旧犬鳴トンネル。その近くには日本政府の統治が及ばない集落“犬鳴村”があり、そこに立ち入った者は決して戻れないという都市伝説がある。果たしてこれは単なる都市伝説なのか、真実なのか…。（C）2020 「犬鳴村」製作委員会 基本情報 監督\t清水崇 出演\t三吉彩花, 坂東龍汰, 古川毅 あらすじ SNSや動画サイト上で恐怖の声が絶えない九州屈指の心霊スポット・旧犬鳴トンネル。 その近くには日本政府の統治が及ばない集落“犬鳴村”があり、そこに立ち入った者は決して戻れないという都市伝説がある。 果たしてこれは単なる都市伝説なのか、真実なのか…。 感想 パッケージ観ると、上手くできてて見たくなりませんか？ しかも恐怖回避バージョンという謎のバージョンが存在します。 これは犬鳴村をそのまま使って、怖くない仕様の可愛い編集を追加しまくった作品です。 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 犬鳴村 SNSや動画サイト上で恐怖の声が絶えない九州屈指の心霊スポット・旧犬鳴トンネル。その近くには日本政府の統治が及ばない集落“犬鳴村”があり、そこに立ち入った者は決して戻れないという都市伝説がある。果たしてこれは単なる都市伝説なのか、真実なのか…。（C）2020 「犬鳴村」製作委員会 結構力が入ってるなって思って「犬鳴村」を観てみました。 そうすると、残念なシーンが多々あります。 理由はやりすぎ演出です。 ジャパニーズ・ホラーって、直接的ではなく雰囲気で怖がらせるような感じですよね？ 「犬鳴村」は、直接演出が多すぎてちょっと興醒めします。 幽霊がかなりガッツリ目で出演します。 中には人間から触れる幽霊も出てきます。 後、もう一つ謎のタイムリープ解説シーン。 過去に起こった悲惨な事件を現在と過去とを重ね合わせて説明しています。 ホラー映画で、これをやられるとどこからが事実なのかわからなくて悪い意味で混乱します。 この映画を観るなら、斬新な「犬鳴村　恐怖回避ばーじょん」の方がおすすめかも知れません。 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 犬鳴村 SNSや動画サイト上で恐怖の声が絶えない九州屈指の心霊スポット・旧犬鳴トンネル。その近くには日本政府の統治が及ばない集落“犬鳴村”があり、そこに立ち入った者は決して戻れないという都市伝説がある。果たしてこれは単なる都市伝説なのか、真実なのか…。（C）2020 「犬鳴村」製作委員会"
 },
 {
-url: "https://book.choihack.com/tags/%E3%82%A2%E3%83%8B%E3%83%A1/",
+url: "https://review.choihack.com/tags/%E3%82%A2%E3%83%8B%E3%83%A1/",
 title: "アニメ",
 date: "2021-08-09T21:13:39+09:00",
 body: "アニメ"
 },
 {
-url: "https://book.choihack.com/post/anime/",
+url: "https://review.choihack.com/post/anime/",
 title: "",
 date: "2021-08-09T21:13:39+09:00",
 body: ""
 },
 {
-url: "https://book.choihack.com/post/anime/%E6%9D%B1%E4%BA%AC%E3%83%AA%E3%83%99%E3%83%B3%E3%82%B8%E3%83%A3%E3%83%BC%E3%82%BA/",
+url: "https://review.choihack.com/post/anime/%E6%9D%B1%E4%BA%AC%E3%83%AA%E3%83%99%E3%83%B3%E3%82%B8%E3%83%A3%E3%83%BC%E3%82%BA/",
 title: "東京リベンジャーズ",
 date: "2021-08-09T21:13:39+09:00",
 body: "東京リベンジャーズ 評価 var ctx = document.getElementById(\"raderChart\"); var raderChart = new Chart(ctx, { type: 'radar', data: { labels: [ '意外性','テンポ','面白さ' ], datasets: [{ data: [ 8,8,8 ], backgroundColor: 'RGBA(225,95,150, 0.5)', borderColor: 'RGBA(225,95,150, 1)', borderWidth: 1, pointBackgroundColor: 'RGB(46,106,177)' }] }, options: { legend: { display: false, }, scale:{ ticks:{ suggestedMin: 0, suggestedMax: 10, stepSize: 2 }, pointLabels: { fontSize: 16 } } } }); .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 東京リベンジャーズ 基本情報 出演: 新祐樹, 和氣あず未, 逢坂良太 あらすじ 人生どん底のダメフリーター花垣武道（タケミチ）。 中学時代に付き合っていた人生唯一の恋人・橘日向（ヒナタ）が、最凶最悪の悪党連合”東京卍會”に殺されたことを知る。 事件を知った翌日、駅のホームにいたタケミチは何者かに背中を押され線路に転落し死を覚悟したが、目を開けると何故か12年前にタイムリープしていた。 人生のピークだった12年前の中学時代にタイムリープし、恋人を救うため、逃げ続けた自分を変えるため、人生のリベンジを開始する！ 感想 一言でいうと 「タイムリープ x ヤンキー」 初めは冴えないフリーターの主人公なんです。。。 でも、話が進むにつれ、主人公の男気があがっていきます。 「冴えない主人公が成り上がる」 という王道系の漫画と、 「タイムリープ」 という今までなかった組合わせで展開が面白い。 アニメの14話時点で5巻です。 漫画は最新巻が23巻まで出てるので、まだまだアニメはこれからですね。 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 東京卍リベンジャーズ（２３） (週刊少年マガジンコミックス)"
 },
 {
-url: "https://book.choihack.com/tags/%E8%AC%9B%E8%AB%87%E7%A4%BE/",
+url: "https://review.choihack.com/tags/%E8%AC%9B%E8%AB%87%E7%A4%BE/",
 title: "講談社",
 date: "2021-08-09T21:13:39+09:00",
 body: "講談社"
 },
 {
-url: "https://book.choihack.com/post/movie/%E4%B8%80%E5%BA%A6%E6%AD%BB%E3%82%93%E3%81%A7%E3%81%BF%E3%81%9F/",
+url: "https://review.choihack.com/post/movie/%E4%B8%80%E5%BA%A6%E6%AD%BB%E3%82%93%E3%81%A7%E3%81%BF%E3%81%9F/",
 title: "一度死んでみた",
 date: "2021-08-08T15:45:20+09:00",
 body: "一度死んでみた 評価 var ctx = document.getElementById(\"raderChart\"); var raderChart = new Chart(ctx, { type: 'radar', data: { labels: [ '意外性','テンポ','面白さ' ], datasets: [{ data: [ 1,8,3 ], backgroundColor: 'RGBA(225,95,150, 0.5)', borderColor: 'RGBA(225,95,150, 1)', borderWidth: 1, pointBackgroundColor: 'RGB(46,106,177)' }] }, options: { legend: { display: false, }, scale:{ ticks:{ suggestedMin: 0, suggestedMax: 10, stepSize: 2 }, pointLabels: { fontSize: 16 } } } }); .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 一度死んでみた 父親のことが大嫌い、いまだ反抗期を引きずっている女子大生の七瀬（広瀬すず）。売れないデスメタルバンドでボーカルをしている彼女は、ライブで「一度死んでくれ！」と父・計（堤真一）への不満をシャウトするのが日常だった。そんなある日、計が本当に死んでしまったとの知らせが。実は計が経営する製薬会社で偶然発明された「2日だけ死んじゃう薬」を飲んだためで、計は仮死状態にあるのだった。ところが、計を亡き者にしようとするライバル会社の陰謀で、計は本当に火葬されてしまいそうに…！大嫌いだったはずの父の、絶体絶命のピンチに直面した七瀬は、存在感が無さすぎて“ゴースト”と呼ばれている計の部下・松岡（吉沢亮）とともに、父を救うため立ち上がることを決意する。火葬までのタイムリミットは2日間。はたして七瀬は無事、計を生き返らせることができるのか--！？(Ｃ)2020 松竹 フジテレビジョン 基本情報 監督: 浜崎慎治 出演: 広瀬すず, 吉沢亮, 堤真一 あらすじ 父親のことが大嫌い、いまだ反抗期を引きずっている女子大生の七瀬（広瀬すず）。 売れないデスメタルバンドでボーカルをしている彼女は、ライブで「一度死んでくれ！」と父・計（堤真一）への不満をシャウトするのが日常だった。 そんなある日、父が本当に死んでしまったとの知らせが。 実は計が経営する製薬会社で偶然発明された「2日だけ死んじゃう薬」を飲んだためで、計は仮死状態にあるのだった。 ところが、計を亡き者にしようとするライバル会社の陰謀で、計は本当に火葬されてしまいそうに…！ 大嫌いだったはずの父の、絶体絶命のピンチに直面した七瀬は、存在感が無さすぎて“ゴースト”と呼ばれている計の部下・松岡（吉沢亮）とともに、父を救うため立ち上がることを決意する。 火葬までのタイムリミットは2日間。 はたして七瀬は無事、計を生き返らせることができるのか\u0026ndash;！？ 感想 話はサクサク進んでいくので最後まで見れました。 ですが、途中の理由付けとかは結構無理矢理感があります。 2日だけ死んじゃう薬というアイテム以外は特に意外性がなく、想定通りの結末に向かっていきます。 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 一度死んでみた 父親のことが大嫌い、いまだ反抗期を引きずっている女子大生の七瀬（広瀬すず）。売れないデスメタルバンドでボーカルをしている彼女は、ライブで「一度死んでくれ！」と父・計（堤真一）への不満をシャウトするのが日常だった。そんなある日、計が本当に死んでしまったとの知らせが。実は計が経営する製薬会社で偶然発明された「2日だけ死んじゃう薬」を飲んだためで、計は仮死状態にあるのだった。ところが、計を亡き者にしようとするライバル会社の陰謀で、計は本当に火葬されてしまいそうに…！大嫌いだったはずの父の、絶体絶命のピンチに直面した七瀬は、存在感が無さすぎて“ゴースト”と呼ばれている計の部下・松岡（吉沢亮）とともに、父を救うため立ち上がることを決意する。火葬までのタイムリミットは2日間。はたして七瀬は無事、計を生き返らせることができるのか--！？(Ｃ)2020 松竹 フジテレビジョン"
 },
 {
-url: "https://book.choihack.com/post/book/%E3%82%84%E3%82%8A%E6%8A%9C%E3%81%8F%E4%BA%BA%E3%81%AE%EF%BC%99%E3%81%A4%E3%81%AE%E7%BF%92%E6%85%A3-%E3%82%B3%E3%83%AD%E3%83%B3%E3%83%93%E3%82%A2%E5%A4%A7%E5%AD%A6%E3%81%AE%E6%88%90%E5%8A%9F%E3%81%AE%E7%A7%91%E5%AD%A6/",
+url: "https://review.choihack.com/post/book/%E3%82%84%E3%82%8A%E6%8A%9C%E3%81%8F%E4%BA%BA%E3%81%AE%EF%BC%99%E3%81%A4%E3%81%AE%E7%BF%92%E6%85%A3-%E3%82%B3%E3%83%AD%E3%83%B3%E3%83%93%E3%82%A2%E5%A4%A7%E5%AD%A6%E3%81%AE%E6%88%90%E5%8A%9F%E3%81%AE%E7%A7%91%E5%AD%A6/",
 title: "やり抜く人の９つの習慣　コロンビア大学の成功の科学",
 date: "2021-07-27T07:35:51+09:00",
 body: "やり抜く人の９つの習慣　コロンビア大学の成功の科学 おすすめ度 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } やり抜く人の９つの習慣　コロンビア大学の成功の科学 var ctx = document.getElementById(\"raderChart\"); var raderChart = new Chart(ctx, { type: 'radar', data: { labels: [ '意外性','面白さ','エビデンス' ], datasets: [{ data: [ 7,7,10 ], backgroundColor: 'RGBA(225,95,150, 0.5)', borderColor: 'RGBA(225,95,150, 1)', borderWidth: 1, pointBackgroundColor: 'RGB(46,106,177)' }] }, options: { legend: { display: false, }, scale:{ ticks:{ suggestedMin: 0, suggestedMax: 10, stepSize: 2 }, pointLabels: { fontSize: 16 } } } }); はじめに やりたいことはいっぱいあるので、どうしてもやる気が出ずに先送りしていることはないでしょうか？ Googleカレンダーに登録してもスルー 明日こそはやろうと思っても結局スルー できる人とできない人の違いって、コツコツと小さな達成を積み上げられるかどうかの差なのかもしれません。 DaiGoさんのこの本は決めたことをやり抜くための習慣が多数記載されています。 その中で、すぐにポイントだと感じた内容をまとめます。 if-thenプランニング やり方は簡単です。 if-thenは「もしこうなったら、こうする」という意味ですが、いつどんな行動をするのかを先に決めておくということです。 ポイントは、いつと何をを明確に決めておくことです。 例えば、 もし、朝起きて歯磨きをしたら、その後にシャワーを浴びる もし、勤務時間中に同僚との話がはずんで仕事が進まないなら、5分で切り上げて仕事に戻る もし、平日7時になったら、いつものジムで汗を流す たったこれだけのことで、if-thenプランニングをしない時に比べて2~3倍達成率があがります。 理由は、このif-thenが脳への訴えかけが強いからです。 「XならY」という単純なルールは脳へ記憶しやすく、脳は無意識下でifの条件を探し、決めたタイミングで実行できる可能性があがります。 現実的楽観主義者になる 目標に向かって進んでいるときポジティブに考えることは大切です。 そこで考えるのは「成功を信じよ。そうすれば成功はあなたのものだ。」のようなこと。 確かに、この考え方は非常に正しく、心理学者アルバート・バンデューラも以下のように言っています。 ある人が成功できるかどうかの最も信頼できる指標─それはその人が心から成功できると信じているかどうかにある \u0026ndash; 心理学者アルバート・バンデューラ ですが、これは大きな誤解が潜んでいます。 その誤解とは「現実的な楽観主義者」なのか「非現実的な楽観主義者」なのかということです。 目標が簡単に達成できると考えてはいけないのです。 本当に成功できる人は、成功までの道のりは厳しい・難しいと理解した上で、戦略を練り・へこたれずに努力し続けられる人です。 非現実的な楽観主義者はこれを勘違いして、勝手に向こうから成功がやってくるかのように振る舞います。 これではいつまで立っても成功に向かって動いておらず、目標達成はできません。 まとめると 成功のためには現実的な楽観主義者になる 現実的とは、成功までにある課題を見つめて、その課題がどの程度のものなのかをよく検討すること 固定的知能感と拡張的知能感 成功を掴むためには、自身の持っている能力が影響します。 この能力に対して我々は固定的知能感と拡張的知能感の2種類の捉え方をしています。 固定的知能感とは、自身の能力は固定的で、生まれ持ったものから成長させることはできないという考えです。 拡張的知能感とは、能力とは変更可能で、努力や経験によって後から高めることができるという考えです。 実際、拡張的知能感の方が正しく、能力は後から伸ばすことができます。 ですが、厄介なのは固定的知能感を持っていると、能力の習得を阻害してしまうことです。 「私にはそもそも才能がなかったから、できなくて当然、やるだけ無駄だ」といった考えになってしまいます。 たとえ、その時できないことがあったとしても「今の私にはできないだけ」だと考え方を変えましょう。 意志力 何かに取り組む時に意志の力は絶大です。 あらゆることを達成している人は、どうやって意志力を維持しているのでしょうか？ 意志力にはいくつかの性質があります。 鍛えて高めることができる 限りがあり、意志決定するたびに削られる 自身の意志力を過大評価している これらの性質を踏まえて、成功できるように以下のテクニックがあります。 意志力を回復する 気分があがることをする 休む 自分を褒める 身の回りの鉄の意志を持つ人を思い浮かべる 意志力を鍛える ちょっと嫌なことを繰り返す 毎日身の回りの整理 靴を揃える さいごに 小さな習慣の積み重ねは大きな成功を導きます。 私は毎朝カフェに行き、読書や勉強などを行うという習慣を手に入れることができました。 今振り返ると、この習慣のはこの本で紹介しているいくつかの習慣に当てはまるところがあります。 千里の道も一歩から。 少しづつ確実に前に進んでいきましょう。"
 },
 {
-url: "https://book.choihack.com/post/book/",
+url: "https://review.choihack.com/post/book/",
 title: "",
 date: "2021-07-27T07:35:51+09:00",
 body: ""
 },
 {
-url: "https://book.choihack.com/post/%E8%AA%AD%E3%81%BF%E3%81%9F%E3%81%84%E6%9C%AC%E3%82%92%E3%81%9F%E3%81%A0%E5%88%97%E6%8C%99%E3%81%97%E3%81%9F%E3%81%A0%E3%81%91/",
+url: "https://review.choihack.com/post/%E8%AA%AD%E3%81%BF%E3%81%9F%E3%81%84%E6%9C%AC%E3%82%92%E3%81%9F%E3%81%A0%E5%88%97%E6%8C%99%E3%81%97%E3%81%9F%E3%81%A0%E3%81%91/",
 title: "読みたい本をただ列挙しただけ",
 date: "2021-07-26T08:51:47+09:00",
 body: "読みたい本をただ列挙しただけ はじめに 人に進められた本は読みたくなってしまいます。 ですが、そんな気持ちも時間が経つと摩耗して失われていきます。 読みたい本を忘れないように、読みたいときのモチベーションもなるべく書きつつ列挙するだけの投稿です。 目標達成を最適化する本 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 脳を鍛えるには運動しかない！最新科学でわかった脳細胞の増やし方 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } ハーバード×ＭＢＡ×医師　目標を次々に達成する人の最強の勉強法 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 影響力の武器 戦略編: 小さな工夫が生み出す大きな効果 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 週40時間の自由をつくる 超時間術 心理学 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } ヒルガードの心理学 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 心理学・入門（改訂版） 有斐閣アルマ .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 心理学大図鑑 三省堂大図鑑シリーズ .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 影響力の武器[第三版]　なぜ、人は動かされるのか マーケティング .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } ザ・コピーライティング――心の琴線にふれる言葉の法則 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } シュガーマンのマーケティング30の法則 お客がモノを買ってしまう心理的トリガーとは .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } ハーバード×MIT流 世界最強の交渉術---信頼関係を壊さずに最大の成果を得る6原則 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } ファスト\u0026amp;スロー あなたの意思はどのように決まるか? 文庫 (上)(下)セット 投資系 .amazon-content { overflow: hidden; } .amazon-content .title { font-size: 20px; font-weight: bold; } .amazon-content .description { display: -webkit-box; -webkit-box-orient: vertical; -webkit-line-clamp: 3; overflow: hidden; } 超訳「国富論」―――経済学の原点を２時間で理解する"
 },
 {
-url: "https://book.choihack.com/categories/",
+url: "https://review.choihack.com/categories/",
 title: "Categories",
 date: "0001-01-01T00:00:00Z",
 body: "Categories"
 },
 {
-url: "https://book.choihack.com/post/item/",
+url: "https://review.choihack.com/post/item/",
 title: "",
 date: "0001-01-01T00:00:00Z",
 body: ""
